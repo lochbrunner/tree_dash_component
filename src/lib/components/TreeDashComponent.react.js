6adb00e97062b8b4e81b69ec5d6210cd1446d091
Matthias Lochbrunner <matthias_lochbrunner@web.de>
2019-10-30 21:38:25 +0100

    Frontend draws tree

--- src/lib/components/TreeDashComponent.react.js
+++ src/lib/components/TreeDashComponent.react.js
@@ -1,5 +1,93 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+
+function* traverse_bfs(node) {
+    // Return line by line
+    let even = [];
+    let id = 0;
+    let odd = [{ node, id }];
+
+    while (odd.length > 0) {
+        yield odd;
+        while (odd.length > 0) {
+            const { node } = odd.pop();
+            ++id;
+            // console.log(`odd: ${node.ident}`)
+            even.push(...(node.childs.map(c => ({ node: c, id }))));
+        }
+        if (even.length < 1)
+            break;
+
+        yield even;
+        while (even.length > 0) {
+            const { node } = even.pop()
+            ++id;
+            // console.log(`even: ${node.ident}`)
+            odd.push(...(node.childs.map(c => ({ node: c, id }))));
+        }
+    }
+}
+
+function calcDepth(node) {
+    let depth = 0;
+    while (node) {
+        node = node.childs[0];
+        depth += 1;
+    }
+    return depth;
+};
+
+function padding(a, b) {
+    const PADDING = 10;
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+    const d = Math.sqrt(dx * dx + dy * dy);
+    const mx = PADDING * dx / d;
+    const my = PADDING * dy / d;
+    return { x1: a.x - mx, x2: b.x + mx, y1: a.y - my, y2: b.y + my };
+}
+
+function create_image(symbol) {
+    const depth = calcDepth(symbol);
+    const HEIGHT = 256;
+    const WIDTH = 256;
+    let i = 0;
+    const dict = {};
+    const points = [];
+    const edges = [];
+    let y = HEIGHT / depth / 2;
+    const dy = HEIGHT / depth;
+    for (let line of traverse_bfs(symbol)) {
+        const cx = line.length;
+        let x = WIDTH / cx / 2;
+        const dx = WIDTH / cx;
+        for (let col in line) {
+            ++i;
+            const { node, id } = line[col];
+            points.push(<text key={i} x={x} y={y} textAnchor="middle" dominantBaseline="middle">{node.ident}</text>);
+            dict[i] = { x, y };
+            if (id in dict) {
+                // Draw line
+                const p = dict[id];
+                edges.push(<line key={i} {...padding(p, { x, y })} stroke="black" strokeWidth="2" />);
+            }
+            x += dx;
+        }
+        y += dy;
+    }
+
+    const image = <svg width={WIDTH} height={HEIGHT}>
+        <rect x="0" y="0" width={WIDTH} height={HEIGHT} fill="#eee" />
+        {points}
+        {edges}
+    </svg>;
+
+    return image;
+}
 
 /**
  * ExampleComponent is an example component.
@@ -10,25 +98,18 @@ import PropTypes from 'prop-types';
  */
 export default class TreeDashComponent extends Component {
     render() {
-        const {id, label, setProps, value} = this.props;
+        const { id, label, setProps, value } = this.props;
+
+        const symbol = { ident: 'a', childs: [{ ident: 'b', childs: [] }, { ident: 'c', childs: [] }] };
+        const image = create_image(symbol);
 
         return (
             <div id={id}>
                 ExampleComponent: {label}&nbsp;
+                {image}
                 <input
                     value={value}
-                    onChange={
-                        /*
-                         * Send the new value to the parent component.
-                         * setProps is a prop that is automatically supplied
-                         * by dash's front-end ("dash-renderer").
-                         * In a Dash app, this will update the component's
-                         * props and send the data back to the Python Dash
-                         * app server if a callback uses the modified prop as
-                         * Input or State.
-                         */
-                        e => setProps({ value: e.target.value })
-                    }
+                    onChange={e => setProps({ value: e.target.value })}
                 />
             </div>
         );
